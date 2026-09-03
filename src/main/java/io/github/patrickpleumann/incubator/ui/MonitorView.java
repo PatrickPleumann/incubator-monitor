cafc40c816e0876fdeaab2df2a28e279c0f26319
PatrickPleumann <patrickpleumann@web.de>
2026-09-03 14:23:49 +0200

    empty method for creating vbox added - no use yet

--- src/main/java/io/github/patrickpleumann/incubator/ui/MonitorView.java
+++ src/main/java/io/github/patrickpleumann/incubator/ui/MonitorView.java
@@ -4,6 +4,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
@@ -12,24 +13,33 @@ public class MonitorView
 {
     private static final String HEADER_TITLE = "Incubator Monitor";
     private static final String HEADER_COLOR = "#3C1192";
-
     private final BorderPane root;
 
     public MonitorView()
     {
         root = new BorderPane();
+
         root.setTop(createHeader());
+
+        root.setCenter(createDisplay());
     }
 
     private Label createHeader()
     {
         Label header = new Label(HEADER_TITLE);
         header.setFont(Font.font("System", FontWeight.BOLD, 16));
-        header.setTextFill(Color.WHITE);
+        header.setTextFill(Color.BEIGE);
         header.setStyle("-fx-background-color: " + HEADER_COLOR + ";");
         header.setMaxWidth(Double.MAX_VALUE);
         header.setPadding(new Insets(12));
         return header;
+    }
+
+    private VBox createDisplay()
+    {
+        VBox box = new VBox();
+
+        return box;
     }
 
     public Parent getRoot()
